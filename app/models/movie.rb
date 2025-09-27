6d52647953592d2c993db0375ce6b737ae4c028a
Sanskriti Bansal <sb5215@columbia.edu>
2025-09-27 19:59:28 +0000

    filtering feature added

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,13 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    distinct.pluck(:rating)
+  end
+
+  def self.with_ratings(ratings)
+    if ratings.nil? || ratings.empty?
+      all
+    else
+      where(rating: ratings)
+    end
+  end
 end
